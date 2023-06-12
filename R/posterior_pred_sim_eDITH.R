89471edf8e0ab50e9f9cd9cd138676bb2bd1c612
lucarraro <luca.carraro@hotmail.it>
2023-06-12 10:46:49 +0200

    update

--- R/posterior_pred_sim_eDITH.R
+++ R/posterior_pred_sim_eDITH.R
@@ -22,18 +22,24 @@ posterior_pred_sim_eDITH <- function(x, river, nParamSets = 10000, nDrawsPerPara
     C <- evalConc2_cpp(river, ss, x$source.area, tau, p, "AG")
     for (j in 1:nDrawsPerParamSet){
       if (x$ll.type=="norm"){
-        pps[k, ] <- rnorm(length(x$data$ID), mean = C[x$data$ID], sd = param["sigma"])
+        tmp <- rnorm(length(x$data$ID), mean = C[x$data$ID], sd = param["sigma"])
 
       } else if (x$ll.type=="lnorm"){
-        pps[k, ] <- rlnorm(length(x$data$ID),
+        tmp <- rlnorm(length(x$data$ID),
                            meanlog = log(C[x$data$ID]^2/sqrt(param["sigma"]^2 + C[x$data$ID]^2)),
                            sdlog = sqrt(log(param["sigma"]^2/C[x$data$ID]^2 + 1)))
 
       } else if (x$ll.type=="nbinom"){
-        pps[k, ] <- rnbinom(length(x$data$ID),
+        tmp <- rnbinom(length(x$data$ID),
                             size = C[x$data$ID]/(param["omega"]-1),
                             prob = 1/param["omega"])
       }
+      if (x$no.det){
+        Cstar <- param["Cstar"]
+        tmp <- tmp*rbinom(length(x$data$ID), 1, 1-exp(-tmp/Cstar))
+      }
+
+      pps[k, ] <- tmp
       k <- k + 1
     }
   }
